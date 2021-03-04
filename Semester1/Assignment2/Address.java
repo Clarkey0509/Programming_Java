abd5b44b91095d4f44c232b496bb02763aeb6c6b
Clarkey0509 <57994691+Clarkey0509@users.noreply.github.com>
2021-03-04 12:25:48 +0000

    Create Address.java

---
+++ Semester1/Assignment2/Address.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Address here.
+ *
+ * @author Eoin Clarke
+ * @version V1
+ */
+
+public class Address
+{
+    private String address; //Creating field//
+    
+    public Address(String address)
+    {
+        this.address = address; //Setting field when object is created//
+    }
+    
+    public String getAddress()
+    {
+        return address; //Getting Address//
+    }
+
+}
